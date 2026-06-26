1da2c55fe7c6ef4ab7d114efe81a465e90d59c3a
kkamara <kamaracomputers@gmail.com>
2026-06-26 15:20:39 +0100

    Part 2

--- posts/views.py
+++ posts/views.py
@@ -5,3 +5,7 @@ from .models import Post
 def index(request):
     posts = Post.objects.order_by("-created_at")
     return render(request, "posts/index.html", {"posts": posts})
+
+def post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, "posts/post.html", {"post": post})
